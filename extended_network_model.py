085033ad391ee92f38158012e9d622e87d9a4583
EmilioVallati <93369983+EmilioVallati@users.noreply.github.com>
2022-11-06 13:56:26 +0100

    config file update

--- extended_network_model.py
+++ extended_network_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:148f909972090ce5cb1affb4cb407104b00c51418b569281d882d461dc5d5646
-size 40513
+oid sha256:7380bde9f64bec45103ae94c2e0843fe3143f3bbbee02518d08bbe9ac127e9eb
+size 40518
